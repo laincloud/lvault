5409974e73cac05a1f0830886b993bc8d7fd04a4
liuxu <liuxu623@gmail.com>
2018-04-11 15:31:15 +0800

    fix build error

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 mkdir -p /go/src/github.com/laincloud/
 ln -sf /lain/app /go/src/github.com/laincloud/lvault
+cd /go/src/github.com/laincloud/lvault
 gobuildweb dist
 unzip -o /lain/app/lvault-0.1.zip -d /lain/app
 cd /lain/app/lvault-0.1
